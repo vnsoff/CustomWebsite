f13a9786c65662f00cd76aeb437553678b319c3e
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-05-14 23:51:48 -0300

    Rearranging files and structure

---
+++ custom-website/src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Services from "./Services";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Partners from "./Partners";
+
+export {
+    Navbar,
+    Business,
+    Clients,
+    Footer,
+    Services,
+    Hero,
+    Stats,
+    Partners,
+}
